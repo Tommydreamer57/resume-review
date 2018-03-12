97f18b95521a838e71d097b705d86094a8b428dd
Tommydreamer57 <minilao94@yahoo.com>
2018-03-12 14:20:34 -0600

    auth0 connected

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const Auth0Strategy = require('auth0');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 require('dotenv').config();
+
+// APP
 
 const app = express();
 
+// CONSTANTS
+
 const PORT = 8070;
-const { URI, DBNAME } = process.env;
+const {
+    URI,
+    DBNAME,
+    SECRET,
+    AUTH_DOMAIN,
+    AUTH_CLIENT_ID,
+    AUTH_CLIENT_SECRET,
+    CALLBACK_URL,
+    SUCCESS_REDIRECT,
+    FAILURE_REDIRECT
+} = process.env;
+
+// TOP LEVEL MIDDLEWARE
 
 app.use(bodyParser.json());
 
@@ -24,28 +42,68 @@ const coll = {
     resumes: 'resume-resumes',
     comments: 'resume-comments'
 };
-
+// CONNECTION
 MongoClient.connect(URI, function (err, client) {
     assert.equal(null, err);
     console.log(`${DBNAME} connected to server`);
 
     db = client.db(DBNAME);
-    console.log("DB: ");
-    console.log(db);
-    console.log("CLIENT: ");
-    console.log(client);
+    // console.log("DB: ");
+    // console.log(db);
+    // console.log("CLIENT: ");
+    // console.log(client);
 
     app.listen(PORT, () => console.log(`Resume Review listening on port ${PORT}`));
     // client.close();
 });
 
+// SESSION
+
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
 // AUTH0 CONNECTION
 
-app.use(new Auth0Strategy({
+app.use(passport.initialize());
+app.use(passport.session());
 
-}, function () {
-    
-}))
+passport.use(new Auth0Strategy({
+    domain: AUTH_DOMAIN,
+    clientID: AUTH_CLIENT_ID,
+    clientSecret: AUTH_CLIENT_SECRET,
+    callbackURL: CALLBACK_URL
+}, function (accessToken, refreshToken, extraParams, profile, done) {
+    db.collection(coll.users).find({ id: profile.id }).toArray((err, results) => {
+        console.log(results);
+        if (!results.length) {
+            console.log("ADDING")
+            db.collection(coll.users).save(profile).then(console.log)
+        }
+        else {
+            console.log("FOUND")
+            return done(null, profile.id);
+        }
+        // console.log(profile);
+    });
+}));
+
+// AUTH0 ENDPOINTS
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: SUCCESS_REDIRECT,
+    failureRedirect: FAILURE_REDIRECT
+}));
+
+passport.serializeUser(function (id, done) {
+    return done(null, id);
+});
+passport.deserializeUser(function (id, done) {
+    return done(null, id)
+})
 
 // MY ENDPOINTS HERE
 
@@ -73,5 +131,10 @@ app.get('/individual', (req, res) => {
             console.log(results);
             res.status(200).send(results);
         }
+    });
+});
+app.get('/individual/:name', (req, res) => {
+    db.collection(coll.users).find({ name: req.params.name }).toArray((err, user) => {
+        res.status(200).send(user)
     });
 });
